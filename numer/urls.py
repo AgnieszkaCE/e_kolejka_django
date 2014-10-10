76498b0ac6996d61817ac782fe2a50483a9b86bf
Celina_C <cceellkkaa@gmail.com>
2014-10-11 00:08:32 +0200

    Delete

--- numer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-from numer import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.ZapisView.as_view(), name='numer'),
-    url(r'^pobierz/$', 'numer.views.pobieranienu'),
-    
-)
